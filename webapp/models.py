25041f260c0e3a02aa75a1fac9d84bd28b2776d5
ParanoyaFobios <237629134pah@gmail.com>
2025-08-05 18:06:08 +0300

    waitress added, prepare to dockerize

--- webapp/models.py
+++ webapp/models.py
@@ -4,9 +4,9 @@ class Product(models.Model):
     title = models.CharField(max_length=500, null=True)
     current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     rating = models.FloatField(null=True)
-    reviews = models.IntegerField(null=True)  # Изменил на Integer для количества отзывов
+    reviews = models.IntegerField(null=True)
     product_url = models.URLField(max_length=1000, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)  # Добавим дату создания
+    created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         ordering = ['-created_at']  # Сортировка по дате создания
